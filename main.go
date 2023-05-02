c02c56b3d8532861e0e59e84fb17052164084307
Sean <sean.yang@econommi.io>
2023-05-02 16:23:11 -0700

    manipulating and iterating over maps

--- main.go
+++ main.go
@@ -3,10 +3,44 @@ package main
 import "fmt"
 
 func main() {
+	// ---------------
+	// Manipulating Maps
+	// ---------------
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// 1st way of declaring empty map
+	// var colors map[string]string
+
+	// // 2nd way of declaring empty map
+	// colors := make(map[string]string)
+
+	// // add a key-value pair
+	// colors["white"] = "#fffff"
+	// // delete a key-value pair
+	// delete(colors, "white")
+
+	// fmt.Println(colors)
+
+	// -------------
+	// Iterating over Maps
+	// --------------
+
 	colors := map[string]string{
 		"red":   "#ff0000",
-		"green": "4bf745",
+		"green": "#4bf745",
+		"white": "#fffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
